f8b0faec50750403228ae5b3dc274d8000cc37b6
Gustavo Adolfo <gustavo.adolfo.as@gmail.com>
2026-06-07 15:44:01 -0300

    fix: update cache_table_name reference in lambda module and add missing output for ddb_acervo_cache_name

--- terraform/envs/prod/main.tf
+++ terraform/envs/prod/main.tf
@@ -29,7 +29,7 @@ module "lambda" {
   account_id                    = local.account_id
   region_name                   = local.region
   application_tags              = data.aws_servicecatalogappregistry_application.minhoteca_application.application_tag
-  cache_table_name              = module.database.cache_table_name
+  cache_table_name              = module.database.ddb_acervo_cache_name
   cache_hash_key_attribute_name = module.database.cache_hash_key_attribute_name
   mongodb_username              = var.mongodb_username
   mongodb_password              = var.mongodb_password
